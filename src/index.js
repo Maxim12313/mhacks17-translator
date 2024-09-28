10927594f4d0c960d3a97213cea6008f107e34e1
JeffordShau <shaujeff@gmail.com>
2024-09-28 15:46:44 -0400

    avatar window

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-const { app, BrowserWindow, globalShortcut, ipcMain } = require("electron");
+const { app, BrowserWindow, globalShortcut, ipcMain, screen } = require("electron");
 const path = require("path");
 const deepl = require("deepl-node");
+const { create } = require("domain");
 
 let mainWindow;
 let popupWindow;
+let avatarWindow;
+
+function createAvatarWindow() {
+
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  avatarWindow = new BrowserWindow({
+    width: 200,
+    height: 200,
+    transparent: true,
+    frame: false,
+    skipTaskbar: false,
+    alwaysOnTop: true,
+    resizable: false,
+    hasShadow: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  avatarWindow.setIgnoreMouseEvents(true);
+
+  // Set the overlay position to bottom-right corner
+  avatarWindow.setBounds({
+    x: width - 400, // Right edge position (400 is the width of the overlay)
+    y: height - 300, // Bottom edge position (300 is the height of the overlay)
+    width: 200,
+    height: 200,
+  });
+
+  avatarWindow.loadFile('src/avatar.html');
+
+  avatarWindow.on('closed', () => {
+    avatarWindow = null;
+  });
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -64,11 +103,14 @@ function createPopup() {
 app.whenReady().then(() => {
   createWindow();
   testMaxim();
+  createAvatarWindow();
 
   globalShortcut.register("CommandOrControl+I", togglePopup);
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
 
