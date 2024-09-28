29ae9507d0b967b5dcedbfbe702041d21df59a74
maxim12313 <maximkim16@gmail.com>
2024-09-28 15:42:34 -0400

    added debounce and throttling

--- src/maxim-renderer.js
+++ src/maxim-renderer.js
@@ -2,27 +2,41 @@ const input = document.getElementById("popupInput");
 const output = document.getElementById("translated");
 
 let timeId = -1;
+let prev = 0;
+function throttleDebounce(func, limit) {
+  const curr = new Date().getTime();
+  const diff = curr - prev;
+  if (diff > limit) {
+    prev = curr;
+    func();
+    clearTimeout(timeId);
+    timeId = -1;
+  } else {
+    timeId = setTimeout(() => {
+      prev = new Date().getTime();
+      func();
+      clearTimeout(timeId);
+      timeId = -1;
+    }, limit);
+  }
+}
+
 async function doTranslation() {
   const curr = input.value;
   const language = "fr";
 
   if (curr == "") {
     output.innerText = "";
-    timeId = -1;
     return;
   }
-
   const res = await window.maxim.translateTo(input.value, language);
   if (!res.error) {
     output.innerText = res.text;
   } else {
     console.log(res.error);
   }
-  timeId = -1;
 }
 
 input.addEventListener("input", async (event) => {
-  clearTimeout(timeId);
-  const delay = 150;
-  timeId = setTimeout(doTranslation, delay);
+  throttleDebounce(doTranslation, 500);
 });
