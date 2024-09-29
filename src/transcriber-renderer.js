316e142e1041130e0413ed74199a0259c2ab8257
maxim12313 <maximkim16@gmail.com>
2024-09-29 11:08:01 -0400

    transcription is passing

--- src/transcriber-renderer.js
+++ src/transcriber-renderer.js
@@ -1,3 +1,59 @@
-const text = document.getElementById("transcription");
+async function convertBase64(blob) {
+  const buffer = await blob.arrayBuffer();
+  let binary = "";
+  let bytes = new Uint8Array(buffer);
+  let len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
 
-function update() {}
+async function recordComputerHandler() {
+  const textElement = document.getElementById("transcription");
+
+  let recorder;
+
+  window.transcriber.onStart(async () => {
+    if (recorder) {
+      recorder.resume();
+      return;
+    }
+    const ops = await navigator.mediaDevices.enumerateDevices();
+    console.log(ops);
+    const want = "BlackHole 2ch (Virtual)";
+    let deviceId;
+    ops.forEach((device) => {
+      if (device.kind == "audioinput" && device.label == want) {
+        deviceId = device.deviceId;
+      }
+    });
+    if (!deviceId) {
+      console.log("missing blackhole");
+      return;
+    }
+    console.log("so id is " + deviceId);
+
+    const media = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        deviceId,
+      },
+    });
+
+    recorder = new MediaRecorder(media, { mimeType: "audio/webm" });
+
+    recorder.ondataavailable = async (event) => {
+      if (event.data.size == 0) return;
+      const base64 = await convertBase64(event.data);
+      textElement.innerText = await window.electron.streamTranscribe(base64);
+    };
+
+    recorder.start(250);
+  });
+
+  window.transcriber.onEnd(() => {
+    recorder.pause();
+    // window.electron.stopStreaming();
+  });
+}
+recordComputerHandler();
--- src/translation-preload.js
+++ src/translation-preload.js
@@ -1,8 +1,21 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('electron', {
+contextBridge.exposeInMainWorld("electron", {
   translateTo: async (input, language) =>
     await ipcRenderer.invoke("translate-to", { input, language }),
-  receiveTranslation: (callback) => ipcRenderer.on('send-translation', (event, value) => callback(value)),
-  cartesia: (value) => ipcRenderer.invoke('tts-audio', value),
+
+  receiveTranslation: (callback) =>
+    ipcRenderer.on("send-translation", (event, value) => callback(value)),
+
+  cartesia: (value) => ipcRenderer.invoke("tts-audio", value),
+
+  streamTranscribe: async (base64) =>
+    await ipcRenderer.invoke("stream-transcribe", { base64 }),
+
+  stopStreaming: () => ipcRenderer.send("stop-streaming"),
+});
+
+contextBridge.exposeInMainWorld("transcriber", {
+  onStart: (callback) => ipcRenderer.on("start-stream", callback),
+  onEnd: (callback) => ipcRenderer.on("end-stream", callback),
 });
